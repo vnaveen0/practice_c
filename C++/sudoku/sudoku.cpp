8a73134c28b5e7a3c7560271553e96f623c0628a
vnaveen0 <vnaveen0@gmail.com>
2017-04-30 18:22:03 -0700

    converted sudoku to dynamic arrays

--- C++/sudoku/sudoku.cpp
+++ C++/sudoku/sudoku.cpp
@@ -3,13 +3,21 @@
 #include <sstream>
 #include <string>
 #include <math.h>
+#include <cstdlib>
 #include <assert.h>
+
+
+#ifdef DEBUG
+#define Debug(stuff) std::cout << (stuff);
+#else
+#define Debug(stuff)
+#endif
 
 using namespace std;
 
-#define SIZE 9
+// #define SIZE 9
 
-void print_arr( int arr[][SIZE])
+void print_arr( int **arr, int SIZE)
 {
   int row = 0, col=0;
 
@@ -24,7 +32,7 @@ void print_arr( int arr[][SIZE])
 }
 
 
-bool readsudoku(const char* input_file, int arr[][SIZE])
+bool readsudoku(const char* input_file, int** &arr, int SIZE)
 {
   ifstream inFile(input_file);
   // ifstream inFile("input_sudoku.txt");
@@ -35,6 +43,15 @@ bool readsudoku(const char* input_file, int arr[][SIZE])
   }
 
   int row=0, col=0;
+
+  //Initialize arrays
+  //Initialize rows
+  arr = new int* [SIZE];
+  //initialize columns
+  for (int i=0; i<SIZE;i++)
+  {
+    arr[i] = new int [SIZE];
+  }
 
 
   string s;
@@ -59,7 +76,7 @@ bool readsudoku(const char* input_file, int arr[][SIZE])
   return true;
 }
 
-bool find_unassigned(int arr[][SIZE], int & row, int & col)
+bool find_unassigned(int** arr, int SIZE, int & row, int & col)
 {
   for(row=0;row<SIZE;row++)
   {
@@ -74,7 +91,7 @@ bool find_unassigned(int arr[][SIZE], int & row, int & col)
 
 }
 
-bool is_row_safe( int arr[][SIZE], int num, int row)
+bool is_row_safe( int** arr, int SIZE, int num, int row)
 {
   int col=0;
   for(col=0; col<SIZE; col++)
@@ -86,7 +103,7 @@ bool is_row_safe( int arr[][SIZE], int num, int row)
   // cout<<"ROW SAFE: num: "<<num<<endl;
 }
 
-bool is_col_safe( int arr[][SIZE], int num, int col)
+bool is_col_safe( int** arr, int SIZE, int num, int col)
 {
   int row=0;
   for(row=0; row<SIZE; row++)
@@ -98,7 +115,7 @@ bool is_col_safe( int arr[][SIZE], int num, int col)
   // cout<<"COL SAFE: num: "<<num<<endl;
 }
 
-bool is_box_safe( int arr[][SIZE], int num, int row,  int col)
+bool is_box_safe( int** arr, int SIZE, int num, int row,  int col)
 {
   // cout<<"num= "<<num<<" row: "<<row<<" col: "<<col<<endl;
   int box_size=sqrt(SIZE);
@@ -125,9 +142,9 @@ bool is_box_safe( int arr[][SIZE], int num, int row,  int col)
   
 }
 
-bool is_safe(int arr[][SIZE], int num, int row, int col)
+bool is_safe(int** arr, int SIZE, int num, int row, int col)
 {
-  if(is_box_safe(arr, num, row, col) && is_col_safe(arr,num, col) && is_row_safe(arr, num, row) )
+  if(is_box_safe(arr,SIZE, num, row, col) && is_col_safe(arr,SIZE ,num, col) && is_row_safe(arr,SIZE, num, row) )
       return true;
   else 
     return false;
@@ -136,29 +153,29 @@ bool is_safe(int arr[][SIZE], int num, int row, int col)
 
 //TODO: FIND A WAY TO FIND IF INPUT IS WRONG AND ITS NOT POSSIBLE to solve
 //CAN PUT A CHECK TO SEE IF ALL GIVEN INPUTS ARE BOX SAFE, ROW SAFE AND COL SAFE
-bool sudoku_solver(int arr[][SIZE])
+bool sudoku_solver(int** arr, int SIZE)
 {
   int row=0, col=0;
 
-  cout<<"*********-----------------****************"<<endl;
+  Debug(cout<<"*********-----------------****************"<<endl);
   // find unassigned will set row and col
-  if(find_unassigned(arr, row, col))
+  if(find_unassigned(arr,SIZE, row, col))
   {
-    cout<<"Unassigned row: "<<row<<" col: "<<col<<endl;
+    Debug(cout<<"Unassigned row: "<<row<<" col: "<<col<<endl);
 
     for(int num=1; num<=SIZE;num++)
     {
-      if(is_safe(arr, num, row, col))
+      if(is_safe(arr, SIZE, num, row, col))
       {
-        cout<<"SET: a["<<row<<"]["<<col<<"] = "<<num<<endl;
+        Debug(cout<<"SET: a["<<row<<"]["<<col<<"] = "<<num<<endl);
         arr[row][col] = num;
-        if(sudoku_solver(arr))
+        if(sudoku_solver(arr, SIZE))
           return true;
         else 
         {
           //unassign if next iteration of solver
           //returns false and try next number
-          cout<<"BACKTARCK: arr["<<row<<"]["<<col<<"]"<<endl;
+          Debug(cout<<"BACKTARCK: arr["<<row<<"]["<<col<<"]"<<endl);
           arr[row][col] = 0;
           continue;
         
@@ -176,7 +193,7 @@ bool sudoku_solver(int arr[][SIZE])
   else 
   {
     cout<<"solved Sudoku"<<endl;
-    print_arr(arr);
+    print_arr(arr, SIZE);
     return true;
 
   }
@@ -186,8 +203,15 @@ bool sudoku_solver(int arr[][SIZE])
 
 }
 
-int main()
+int main(int argc, char** argv)
 {
+
+  cout<<"argc: "<<argc<<endl;
+  if(argc != 3) assert(0 && "Please provide 2 arguments, arg[0]= SIZE, arg[1] = input_file (space separated)");
+  cout<<"SIZE of sudoku: "<<argv[1]<<endl;
+  cout<<"Input file: "<<argv[2]<<endl;
+  int SIZE = atoi(argv[1]);
+
 
   int box_size = sqrt(SIZE);
   float box_size2 = sqrt(SIZE);
@@ -195,17 +219,20 @@ int main()
 
 
   //Read SUDOKU FILE
-  int arr[SIZE][SIZE];
-  const char* input_file = "input_sudoku.txt";
-  readsudoku(input_file, arr);
+  // int arr[SIZE][SIZE];
+  // int* arr = new int[SIZE];
+  // const char* input_file = "input_sudoku.txt";
+  int **arr = NULL;
+  const char* input_file = argv[2]; 
+  readsudoku(input_file, arr, SIZE);
   
 
   //Print SUDOKU FILE
   cout<<"Initial Sudoku \n";
-  print_arr(arr);
+  print_arr(arr, SIZE);
 
 
-  sudoku_solver(arr);
+  sudoku_solver(arr, SIZE);
   
 
 
